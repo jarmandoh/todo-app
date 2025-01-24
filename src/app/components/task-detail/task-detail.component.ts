3c244fa56acce66fa17d5e339a6d63158507a2e7
janier hernandez <janier007@gmail.com>
2025-01-24 18:48:37 -0500

    details view

--- src/app/components/task-detail/task-detail.component.ts
+++ src/app/components/task-detail/task-detail.component.ts
@@ -4,10 +4,12 @@ import { ActivatedRoute } from '@angular/router';
 import { Task } from '../../models/task.model';
 import { JsonPipe } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-task-detail',
   imports: [
-    JsonPipe
+    MatButtonModule
   ],
   templateUrl: './task-detail.component.html',
   styleUrl: './task-detail.component.css'
@@ -21,6 +23,15 @@ export class TaskDetailComponent {
       //this.taskService.getTask(id).subscribe(task => this.task = task);
       this.task = this.taskService.tasks.filter(task => task.id === id)[0];
     });
-
   }
+  onToggle(){
+    this.task.completed = !this.task.completed;
+  }
+  onDelete(){
+    this.taskService.deleteTask(this.task.id!);
+  }
+  onEdit(){
+    this.taskService.updateTask(this.task);
+  }
+
 }
